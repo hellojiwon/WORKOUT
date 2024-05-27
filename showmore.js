186229b7d2ca468361da03f0560922adc8f71931
hellojiwon <kgwball@naver.com>
2024-05-27 21:51:10 +0900

    Update showmore.js

--- showmore.js
+++ showmore.js
@@ -35,3 +35,29 @@ const btn = document.getElementById('imgbtn');
       btn.click();
 
 
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+                mapOption = { 
+                    center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+                    level: 3 // 지도의 확대 레벨
+            };
+            // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+            var map = new kakao.maps.Map(mapContainer, mapOption); 
+            var markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667); 
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+marker.setMap(map);
+
+    //   modal
+      document.getElementById("subbtn").addEventListener("click", function(event) {
+          event.preventDefault(event); 
+          document.getElementById("mod").style.display = "block";
+      });
+  
+      document.getElementById("modalbtn").addEventListener("click", function(event) {
+          // event.preventDefault(); 
+          document.getElementById("mod").style.display = "none";
+      });
